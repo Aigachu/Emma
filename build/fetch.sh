f4fba5857ec1db6f2a83a6916befdf659c8b8ccf
Aigachu <aigachu.sama@gmail.com>
2019-10-29 10:58:00 -0400

    Added a handy fetch script.

---
+++ build/fetch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#
+# Quick script to fetch bots folder from Luxanna.
+#
+
+# Download the bots folder from Luxanna.
+scp -r aigachu@aigachu.com:~/apps/Estrella/lavenza/bots ./;
