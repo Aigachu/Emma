5af145994b0bb507b1c2f0f2865bc9efb2238fd7
Aigachu <aigachu.sama@gmail.com>
2019-10-28 22:02:05 -0400

    Initial commit.

---
+++ estrella.js
@@ -0,0 +1,9 @@
+// Require the module.
+const Lavenza = require('lavenza');
+
+// Initialize Lavenza.
+// This will read everything found in your lavenza installation folder and do what's necessary to run your bots.
+// Initialize must always run before summoning.
+Lavenza.initialize().then(() => {
+  Lavenza.summon();
+});
